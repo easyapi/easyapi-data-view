35f813a9932a0bb8094e7c4a2effeb76c042d83e
jinkin1995 <jinkin1995@gmail.com>
2019-12-24 00:47:27 +0800

    feat: 更新packageJson

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ Vue.use(Checkbox)
 const install = (Vue) => {
   if (install.installed) return
 
-  Vue.component('el-json-view', JsonView)
+  Vue.component('ea-json-view', JsonView)
 }
 
 export default install
