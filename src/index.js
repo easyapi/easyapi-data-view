b64dd1d2ab1f86f7ac06446d5419ba448dee6742
leida <zhang20084@126.com>
2021-05-18 15:16:59 +0800

    build:第三方包版本升级

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import JsonView from './JsonView.vue'
 import PackageFile from '../package.json'
 import {Checkbox} from "element-ui"
 
-const VERSION = PackageFile.version
+const VERSION = PackageFile.version;
 
-Vue.use(Checkbox)
+Vue.use(Checkbox);
 
 const install = (Vue) => {
-  if (install.installed) return
+  if (install.installed) return;
 
   Vue.component('easyapi-json-view', JsonView)
-}
+};
 
 export default install
 
