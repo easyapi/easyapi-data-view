d645cf6c9c9180eb8d27a7f2048fac412a8ef869
chenyun123 <1537299563@qq.com>
2022-02-24 10:29:02 +0800

    删除jquery

--- webpack.base.js
+++ webpack.base.js
@@ -1,5 +1,5 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const webpack = require('webpack');
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const webpack = require("webpack");
 
 const config = {};
 
@@ -7,63 +7,60 @@ config.module = {
   rules: [
     {
       test: /\.vue$/,
-      loader: 'vue-loader',
+      loader: "vue-loader",
     },
     {
       test: /\.m?js$/,
       exclude: /(node_modules|bower_components)/,
       use: {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-proposal-object-rest-spread']
-        }
-      }
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-object-rest-spread"],
+        },
+      },
     },
     {
       test: /\.css$/,
-      use: [
-        'vue-style-loader',
-        'css-loader'
-      ]
+      use: ["vue-style-loader", "css-loader"],
     },
     {
       test: /\.less$/,
-      use: [{
-        loader: "style-loader"
-        // 将 JS 字符串生成为 style 节点
-      }, {
-        loader: "css-loader"
-        // 将 CSS 转化成 CommonJS 模块
-      }, {
-        loader: "less-loader"
-      }]
+      use: [
+        {
+          loader: "style-loader",
+          // 将 JS 字符串生成为 style 节点
+        },
+        {
+          loader: "css-loader",
+          // 将 CSS 转化成 CommonJS 模块
+        },
+        {
+          loader: "less-loader",
+        },
+      ],
     },
     {
       test: /\.(png|jpg|gif|webp)$/,
-      loader: 'file-loader',
+      loader: "file-loader",
       options: {
-        name: '[name].[ext]?[hash]',
+        name: "[name].[ext]?[hash]",
       },
     },
     {
       test: /\.svg/,
-      loader: 'url-loader',
-    }, {
+      loader: "url-loader",
+    },
+    {
       test: /\.(woff2?|woff|eot|ttf|otf)(\?.*)?$/,
-      loader: 'url-loader',
+      loader: "url-loader",
       options: {
-        name: '[name].[ext]?[hash]'
-      }
-    }
-  ]
+        name: "[name].[ext]?[hash]",
+      },
+    },
+  ],
 };
 
-config.plugins = [
-  new VueLoaderPlugin(),
-  new webpack.ProvidePlugin({
-    $: "jquery"
-  })
-];
+config.plugins = [new VueLoaderPlugin()];
 
 module.exports = config;
